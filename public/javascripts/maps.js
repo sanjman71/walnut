b3bc5b05a0447b35eba8997b58714e1aa62e8607
Sanjay <sanjay@jarna.com>
2009-02-13 11:03:08 -0600

    minor refactoring

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   var lat = $('#map').attr('lat');
   var lng = $('#map').attr('lng');
-  var streetMapCenter = [lat, lng];
   
   $('#map').jmap('init', {'mapType':'map','mapCenter':[lat, lng], 'mapZoom':15});
   $('#map').jmap('AddMarker', {'pointLatLng': [lat, lng]});
-  // $('#map').jmap('CreateStreetviewPanorama', {'latlng':streetMapCenter});
   
   $("#add_streetview").click(function() {
+    var streetMapCenter = [lat, lng];
     $('#map').jmap('CreateStreetviewPanorama', {'latlng':streetMapCenter});
     return false;
   })
