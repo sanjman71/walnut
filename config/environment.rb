2c6e6a2fc6df31cccf691d99c25133262ee6459f
Killian Murphy <killian@killianmurphy.com>
2009-04-25 19:37:38 -0500

    changed haml version to allow minor version changes

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml", :version => '2.0.6'
+  config.gem "haml", :version => '~> 2.0.6'
   config.gem 'mislav-will_paginate', :version => '~> 2.3.6', :lib => 'will_paginate', :source => "http://gems.github.com"
   config.gem 'rubyist-aasm', :version => '~> 2.0.2', :lib => 'aasm', :source => "http://gems.github.com"
   config.gem "geokit" # required by geokit-rails plugin
