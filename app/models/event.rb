6fc80a369dbb2fee247e3bf8f988eb60d088fa67
Sanjay <sanjay@jarna.com>
2009-04-27 10:50:54 -0500

    added event model to sphinx index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,23 @@
 class Event < ActiveRecord::Base
   validates_presence_of     :name, :event_venue_id, :source_type, :source_id
   validates_uniqueness_of   :source_id, :scope => :source_type
+
   belongs_to                :event_venue, :counter_cache => :events_count
+  has_one                   :location, :through => :event_venue
+
+  delegate                  :country, :to => '(location or return nil)'
+  delegate                  :state, :to => '(location or return nil)'
+  delegate                  :city, :to => '(location or return nil)'
+  delegate                  :zip, :to => '(location or return nil)'
+  delegate                  :neighborhoods, :to => '(location or return nil)'
+
+  define_index do
+    indexes name, :as => :name
+    # locality attributes, all faceted
+    has location.country(:id), :type => :integer, :as => :country_id, :facet => true
+    has location.state(:id), :type => :integer, :as => :state_id, :facet => true
+    has locaton.city(:id), :type => :integer, :as => :city_id, :facet => true
+    has location.zip(:id), :type => :integer, :as => :zip_id, :facet => true
+    has location.neighborhoods(:id), :as => :neighborhood_ids, :facet => true
+  end
 end
