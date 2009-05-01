cac19dbb95600ad59df913bf6ce8bb818b25a1c5
Sanjay <sanjay@jarna.com>
2009-05-01 13:02:53 -0500

    removed UserSharedAuthentication module from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
-  include UserSharedAuthentication
   include Authentication
   include Authentication::ByPassword
   include Authentication::ByCookieToken
