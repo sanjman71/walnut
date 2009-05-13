dab700d81548aec3689f24429d01abe84676be28
Sanjay <sanjay@jarna.com>
2009-05-13 11:48:28 -0500

    changed tags column view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,14 +3,15 @@ class TagsController < ApplicationController
   privilege_required 'read tags', :only => [:index], :on => :current_user
   
   def index
-    @search = params[:search].to_s
-    @order  = "taggings_count DESC"
+    @search   = params[:search].to_s
+    order     = "taggings_count DESC"
+    per_page  = 30
     
     case @search
     when ""
-      @tags         = Tag.all(:order => @order).paginate(:page => params[:page], :per_page => 20)
+      @tags         = Tag.all(:order => order).paginate(:page => params[:page], :per_page => per_page)
     else
-      @tags         = Tag.all(:conditions => ["name REGEXP '%s'", @search], :order => @order)
+      @tags         = Tag.all(:conditions => ["name REGEXP '%s'", @search], :order => order)
       @search_text  = "#{@tags.size} Tags matching '#{@search}'"
     end
     
