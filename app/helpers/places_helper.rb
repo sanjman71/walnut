0405205fc55c7487b23e6a9f7ab76d2940b97c29
Sanjay <sanjay@jarna.com>
2009-06-20 11:48:04 -0500

    changed state name to state code in address listings

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,7 +1,7 @@
 module PlacesHelper
 
   def city_state_zip(city, state, zip)
-    [city ? "#{city.name}," : "", state ? state.name : "", zip ? zip.name : ""].compact.join(" ")
+    [city ? "#{city.name}," : "", state ? state.code : "", zip ? zip.name : ""].compact.join(" ")
   end
   
 end
