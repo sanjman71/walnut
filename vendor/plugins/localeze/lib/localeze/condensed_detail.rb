85ddc3592fc02dd43c8c33464c41df3798282f71
Sanjay <sanjay@jarna.com>
2009-06-27 14:12:35 -0500

    updated localeze plugin

--- vendor/plugins/localeze/lib/localeze/condensed_detail.rb
+++ vendor/plugins/localeze/lib/localeze/condensed_detail.rb
@@ -2,5 +2,8 @@ module Localeze
   class CondensedDetail <ActiveRecord::Base
     establish_connection("localeze_#{RAILS_ENV}")
     validates_presence_of :name
+
+    has_many      :company_headings, :class_name=> "Localeze::CompanyHeading"
+    has_many      :base_records, :through => :company_headings
   end
 end
--- vendor/plugins/localeze/lib/localeze/normalized_detail.rb
+++ vendor/plugins/localeze/lib/localeze/normalized_detail.rb
@@ -2,5 +2,10 @@ module Localeze
   class NormalizedDetail <ActiveRecord::Base
     establish_connection("localeze_#{RAILS_ENV}")
     validates_presence_of :name
+
+    has_many      :company_headings, :class_name=> "Localeze::CompanyHeading"
+    has_many      :base_records, :through => :company_headings
+    
+    named_scope   :search_name,           lambda { |s| {:conditions => ["name REGEXP '%s'", s] }}
   end
 end
