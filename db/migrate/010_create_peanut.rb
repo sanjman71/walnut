58d1701ea06d1802c2ef3be4c8f39eddda45140b
Killian Murphy <killian@killianmurphy.com>
2009-07-05 02:17:46 -0700

    consolidated all peanut migrations into a single migration file

--- db/migrate/010_create_peanut.rb
+++ db/migrate/010_create_peanut.rb
@@ -134,24 +134,93 @@ class CreatePeanut < ActiveRecord::Migration
     end
     
     add_index :invitations, :token
+
+    create_table :locatables_locations do |t|
+      t.references :location
+      t.references :locatable, :polymorphic => true
+    end
+
+    add_index :locatables_locations, [:location_id]
+    add_index :locatables_locations, [:locatable_id, :locatable_type], :name => "index_on_locatables"
+    add_index :locatables_locations, [:location_id, :locatable_id, :locatable_type], :name => "index_on_locations_locatables"
+
+    create_table :plans do |t|
+      t.string      :name
+      t.boolean     :enabled
+      t.string      :icon
+      t.integer     :cost   # value in cents
+      t.string      :cost_currency
+      t.integer     :max_locations
+      t.integer     :max_providers
+      t.integer     :start_billing_in_time_amount   # e.g. 1, 5, 30
+      t.string      :start_billing_in_time_unit     # e.g. days, months
+      t.integer     :between_billing_time_amount    # e.g. 1, 5, 30
+      t.string      :between_billing_time_unit      # e.g. days, months
+
+      t.timestamps
+    end
+
+    create_table :payments do |t|
+      t.references  :subscription
+      t.string      :description 
+      t.integer     :amount
+      t.string      :state, :default => 'pending'
+      t.boolean     :success 
+      t.string      :reference 
+      t.string      :message 
+      t.string      :action 
+      t.text        :params 
+      t.boolean     :test
+      t.timestamps
+    end  
+    
+    create_table :subscriptions do |t|
+      t.references  :user
+      t.references  :company
+      t.references  :plan
+      t.datetime    :start_billing_at
+      t.datetime    :last_billing_at
+      t.datetime    :next_billing_at
+      t.integer     :paid_count, :default => 0
+      t.integer     :billing_errors_count, :default => 0
+      t.string      :vault_id, :default => nil
+      t.string      :state, :default => 'initialized'
+      t.timestamps
+    end
+
+    create_table :log_entries do |t|
+      t.references  :loggable, :polymorphic => true  # e.g. appointment
+      t.references  :company, :null => false            # company this is relevant to
+      t.references  :location                           # company location this is relevant to, if any
+      t.references  :customer                           # customer this is relevant to, if any
+      t.references  :user                               # user who created the log_entry
+      t.text        :message_body                       # message body
+      t.integer     :message_id                         # one of the standard message IDs
+      t.integer     :etype                              # informational, approval, urgent
+      t.string      :state
+      t.timestamps
+    end
+
   end
 
   def self.down
-    drop_table :invitations
-    drop_table :notes_subjects
-    drop_table :notes
-    drop_table :invoices
-    drop_table :invoice_line_items
-    drop_table :appointments
-    drop_table :resources
-    drop_table :service_providers
-    drop_table :company_providers
-    drop_table :products
-    drop_table :company_services
-    drop_table :services
-    drop_table :companies
+    drop_table  :log_entries
+    drop_table  :subscriptions
+    drop_table  :payments
+    drop_table  :plans
+    drop_table  :locatables_locations
+    drop_table  :invitations
+    drop_table  :notes_subjects
+    drop_table  :notes
+    drop_table  :invoices
+    drop_table  :invoice_line_items
+    drop_table  :appointments
+    drop_table  :resources
+    drop_table  :service_providers
+    drop_table  :company_providers
+    drop_table  :products
+    drop_table  :company_services
+    drop_table  :services
+    drop_table  :companies
   end
 end
-
-
-
