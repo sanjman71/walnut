a4ab170efc347bcb61f177e177871f8994d6aee2
Killian Murphy <killian@killianmurphy.com>
2009-07-13 17:48:58 -0500

    changed name of event_stream class from EventStream::Event to EventStream. Changed only reference in a rake task

--- app/models/event_stream.rb
+++ app/models/event_stream.rb
@@ -1,28 +1,24 @@
 require 'eventful/api'
 
-module EventStream
+class EventStream
 
-  class Event
-  
-    @@method = "events/search"
+  @@method = "events/search"
 
-    def self.session
-      @@session ||= Eventful::API.new(EVENTFUL_API_KEY)
-    end
+  def self.session
+    @@session ||= Eventful::API.new(EVENTFUL_API_KEY)
+  end
 
-    # search events using the eventful api, with options:
-    #  - :keywords
-    #  - :category => category id or name
-    #  - :location => e.g. "Chicago"
-    #  - :date => "All", "Future", "Past", "Today", "Last Week", "This Week", "Next week"
-    #          => months by name, e.g. 'October'
-    #          => exact ranges take the form 'YYYYMMDDHH-YYYYMMDDHH', e.g. '2008042500-2008042723'
-    #  - :sort_order => 'popularity', 'date', 'title', 'relevance', or 'venue_name', default is 'popularity'
-    def self.search(options={})
-      search_options = {:date => 'Future'}
-      session.call(@@method, search_options.update(options))
-    end
+  # search events using the eventful api, with options:
+  #  - :keywords
+  #  - :category => category id or name
+  #  - :location => e.g. "Chicago"
+  #  - :date => "All", "Future", "Past", "Today", "Last Week", "This Week", "Next week"
+  #          => months by name, e.g. 'October'
+  #          => exact ranges take the form 'YYYYMMDDHH-YYYYMMDDHH', e.g. '2008042500-2008042723'
+  #  - :sort_order => 'popularity', 'date', 'title', 'relevance', or 'venue_name', default is 'popularity'
+  def self.search(options={})
+    search_options = {:date => 'Future'}
+    session.call(@@method, search_options.update(options))
+  end
 
-  end # class
-
-end # module
+end # class
