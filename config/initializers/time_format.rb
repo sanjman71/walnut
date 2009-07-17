acc74f267688d5c23bc0fa4e0f0342760a99cc6b
Sanjay <sanjay@jarna.com>
2009-07-17 17:42:04 -0500

    added time_format in initializers

--- config/initializers/time_format.rb
+++ config/initializers/time_format.rb
@@ -1,5 +1,14 @@
-Time::DATE_FORMATS[:week_day_short]       = "%a"     # Fri
-Time::DATE_FORMATS[:month_short]          = "%b"     # Mar
-Time::DATE_FORMATS[:month_day]            = "%d"     # 31
+Time::DATE_FORMATS[:appt_date_time]             = "%B %d, %Y at %I:%M %p"         # October 31, 2008 at 5:15 pm
+Time::DATE_FORMATS[:appt_day]                   = "%A, %B %d, %Y"                 # Friday, October 31, 2008
+Time::DATE_FORMATS[:appt_day_date_time]         = "%A, %B %d, %Y at %I:%M %p"     # Friday, October 31, 2008 at 5:15 pm
+Time::DATE_FORMATS[:appt_time]                  = "%I:%M %p"                      # 5:15 pm
+Time::DATE_FORMATS[:appt_time_army]             = "%H%M"                          # 1715
+Time::DATE_FORMATS[:appt_schedule]              = "%Y%m%dT%H%M%S"                 # 20081031T120000
+Time::DATE_FORMATS[:appt_schedule_day]          = "%Y%m%d"                        # 20081031
+Time::DATE_FORMATS[:appt_short_month_day]       = "%b %d"                         # Oct 31
+Time::DATE_FORMATS[:appt_short_month_day_year]  = "%b %d %Y"                      # Oct 31 2009
+
+Time::DATE_FORMATS[:appt_week_day_short]        = "%a"                            # Fri
+Time::DATE_FORMATS[:appt_month_day]             = "%d"                            # 31
 
 
