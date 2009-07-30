d8313f1a4734c0bbbc2139115ba89b60c5ddf029
Killian Murphy <killian@killianmurphy.com>
2009-07-29 17:16:06 -0700

    added cache key generation for collections of capacity slots

--- app/models/cache_key.rb
+++ app/models/cache_key.rb
@@ -13,4 +13,17 @@ class CacheKey
     cache_key = date_key + ":" + appt_keys
   end
   
+  # Create cache key for a collection of appointments over a specified daterange
+  def self.slot_schedule(daterange, slots, time_of_day)
+    # use daterange start and end dates
+    date_key  = "#{daterange.start_at.to_s(:appt_schedule)}:#{daterange.end_at.to_s(:appt_schedule)}"
+    date_key  += ":#{time_of_day}" if !time_of_day.blank?
+    
+    # add appointment info using an md5 hash
+    slot_keys = Digest::MD5.hexdigest(slots.collect { |s| s.cache_key }.join)
+    
+    # build cache key
+    cache_key = date_key + ":" + slot_keys
+  end
+
 end
