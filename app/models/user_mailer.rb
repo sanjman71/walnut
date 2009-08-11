868ce1b7e258b574089f17ec732cd7eea36d2b6e
Sanjay <sanjay@jarna.com>
2009-08-11 14:24:59 -0500

    changed user_mailer model

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,24 +1,30 @@
 class UserMailer < ActionMailer::Base
   
   def account_created(company, user, creator, password, login_url)
-    from("peanut@jarna.com")
+    from("messaging@walnutindustries.com")
     recipients(user.email)
     subject("#{company.name}: Your user account was created")
     body(:user => user, :creator => creator, :password => password, :login_url => login_url)
   end
   
   def account_reset(company, user, password, login_url)
-    from("peanut@jarna.com")
+    from("messaging@walnutindustries.com")
     recipients(user.email)
     subject("#{company.name}: Your account password has been reset")
     body(:user => user, :password => password, :login_url => login_url)
   end
   
   def message(company, user, message)
-    from("peanut@jarna.com")
+    from("messaging@walnutindustries.com")
     recipients(user.email)
     subject("#{company.name}: message")
     body(:message => message)
   end
   
+  def email(to, subject, body)
+    from("messaging@walnutindustries.com")
+    recipients(to)
+    subject(subject)
+    body(:body => body)
+  end
 end
