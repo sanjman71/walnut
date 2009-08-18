c4ce95b9b228cf35d87a4b56566b51f9ddc81845
Sanjay <sanjay@jarna.com>
2009-08-18 18:52:16 -0500

    updated crontab recipe

--- config/recipes/crontab.rb
+++ config/recipes/crontab.rb
@@ -3,6 +3,6 @@
 namespace :crontab do
   desc "Update crontab"
   task :update, :roles => :app do
-    run "cd #{current_path} && whenever --update-crontab #{application}"
+    run "cd #{current_path}; whenever --update-crontab #{application}"
   end
 end
