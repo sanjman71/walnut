5c6d8e1e131fd5b08cceaa0c2237277e40853455
Sanjay <sanjay@jarna.com>
2009-12-12 10:35:43 -0800

    added more sitemap index.locations

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,6 +2,7 @@ class SitemapsController < ApplicationController
   caches_page :events
   caches_page :tags
   caches_page :locations
+  caches_page :index_locations
 
   layout nil # turn off layouts
   
@@ -40,17 +41,38 @@ class SitemapsController < ApplicationController
   end
 
   # GET /sitemap.index.locations.il.chicago.xml
+  # GET /sitemap.index.locations.cities.medium.xml
   def index_locations
     @country    = Country.us
-    @state      = @country.states.find_by_code(params[:state])
-    @city       = @state.cities.find_by_name(params[:city].titleize)
+    @city_size  = params[:city_size]
+    
+    case @city_size
+    when 'medium', 'small', 'tiny'
+      # range is number of locations, which is based on the city size
+      case @city_size
+      when 'medium'
+        conditions = ["cities.locations_count < 25000 AND cities.locations_count >= 5000"]
+      when 'small'
+        conditions = ["cities.locations_count < 5000 AND cities.locations_count >= 1000"]
+      when 'tiny'
+        conditions = ["cities.locations_count < 1000"]
+      end
+      @count      = Location.count(:offset => @offset, :limit => @@urls_per_sitemap, :joins => :city, :conditions => conditions)
+      @iend       = @count/@@urls_per_sitemap + ((@count % @@urls_per_sitemap) == 0 ? 0 : 1)
+      @range      = Range.new(1, [@iend, @@entries_per_index].min)
+      # build sitemap root to this range of cities
+      @root       = "/sitemap.locations.cities.#{@city_size}."
+    else
+      # use specified state, city
+      @state      = @country.states.find_by_code(params[:state])
+      @city       = @state.cities.find_by_name(params[:city].titleize)
+      # figure out how many sitemaps are needed for this city based on locations count
+      @iend       = @city.locations_count/@@urls_per_sitemap + ((@city.locations_count % @@urls_per_sitemap) == 0 ? 0 : 1)
+      @range      = Range.new(1, [@iend, @@entries_per_index].min)
+      # build sitemap root to this city
+      @root       = "/sitemap.locations.#{@state.code.downcase}.#{@city.name.to_url_param}."
+    end
 
-    # figure out how many sitemaps are needed for this city based on locations count
-    @iend       = @city.locations_count/@@urls_per_sitemap + ((@city.locations_count % @@urls_per_sitemap) == 0 ? 0 : 1)
-    @range      = Range.new(1, [@iend, @@entries_per_index].min)
-
-    # build sitemap root to this city
-    @root       = "/sitemap.locations.#{@state.code.downcase}.#{@city.name.to_url_param}."
 
     @protocol   = self.request.protocol
     @host       = self.request.host
@@ -74,18 +96,12 @@ class SitemapsController < ApplicationController
 
     case @city_size
     when 'medium' # ~ 187K locations
-      # @locations  = Location.count(:joins => :city, :conditions => ["cities.locations_count < 25000 AND cities.locations_count >= 5000"])
-      # raise Exception, "medium locations: #{@locations}"
       conditions  = ["cities.locations_count < 25000 AND cities.locations_count >= 5000"]
       @locations  = Location.all(:offset => @offset, :limit => @@urls_per_sitemap, :joins => :city, :conditions => conditions)
     when 'small' # ~ 773K locations
-      # @locations  = Location.count(:joins => :city, :conditions => ["cities.locations_count < 5000 AND cities.locations_count >= 1000"])
-      # raise Exception, "small locations: #{@locations}"
       conditions  = ["cities.locations_count < 5000 AND cities.locations_count >= 1000"]
       @locations  = Location.all(:offset => @offset, :limit => @@urls_per_sitemap, :joins => :city, :conditions => conditions)
     when 'tiny' # ~ 440K locations
-      # @locations  = Location.count(:joins => :city, :conditions => ["cities.locations_count < 1000"])
-      # raise Exception, "tiny locations: #{@locations}"
       conditions  = ["cities.locations_count < 1000"]
       @locations  = Location.all(:offset => @offset, :limit => @@urls_per_sitemap, :joins => :city, :conditions => conditions)
     else
