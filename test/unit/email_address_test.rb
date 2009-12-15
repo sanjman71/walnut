6b1c56961520c53b393681e29b00eb8508acf8d4
Sanjay <sanjay@jarna.com>
2009-12-14 22:04:36 -0800

    added email tests

--- test/unit/email_address_test.rb
+++ test/unit/email_address_test.rb
@@ -22,24 +22,55 @@ class EmailAddressTest < ActiveSupport::TestCase
     end
 
     context "with emailable" do
-      setup do
-        @user   = Factory(:user)
-        @email  = @user.email_addresses.create(:address => "a@b.com")
-      end
+      context "for regular user" do
+        setup do
+          @user   = Factory(:user)
+          @email  = @user.email_addresses.create(:address => "a@b.com")
+        end
 
-      should_change("EmailAddress.count") { EmailAddress.count }
+        should_change("EmailAddress.count") { EmailAddress.count }
       
-      should "have priority of 1" do
-        assert_equal 1, @email.priority
-      end
+        should "have priority of 1" do
+          assert_equal 1, @email.priority
+        end
       
-      should "increment user.email_addresses_count" do
-        @user.reload
-        assert_equal 1, @user.email_addresses_count
-      end
+        should "increment user.email_addresses_count" do
+          @user.reload
+          assert_equal 1, @user.email_addresses_count
+        end
 
-      should "have state 'unverified'" do
-        assert_equal 'unverified', @email.state
+        should "have state 'unverified'" do
+          assert_equal 'unverified', @email.state
+        end
+
+        should "be changeable" do
+          assert_true @email.changeable?
+        end
+      end
+      
+      context "for rpx user" do
+        setup do
+          @user   = Factory(:user, :rpx => 1)
+          @email  = @user.email_addresses.create(:address => "a@b.com")
+        end
+        should_change("EmailAddress.count") { EmailAddress.count }
+      
+        should "have priority of 1" do
+          assert_equal 1, @email.priority
+        end
+      
+        should "increment user.email_addresses_count" do
+          @user.reload
+          assert_equal 1, @user.email_addresses_count
+        end
+
+        should "have state 'unverified'" do
+          assert_equal 'unverified', @email.state
+        end
+
+        should "not be changeable" do
+          assert_false @email.changeable?
+        end
       end
     end
   end
