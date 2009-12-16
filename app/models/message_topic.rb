3a254461012945d381cc4432dfe61b009714beff
Sanjay <sanjay@jarna.com>
2009-12-16 10:07:18 -0800

    added message_topic named_scope

--- app/models/message_topic.rb
+++ app/models/message_topic.rb
@@ -3,4 +3,6 @@ class MessageTopic < ActiveRecord::Base
   belongs_to              :topic, :polymorphic => true
   validates_presence_of   :message_id, :topic_id, :topic_type
   validates_uniqueness_of :message_id, :scope => [:topic_id, :topic_type]
+
+  named_scope :for_type,  lambda { |o| { :conditions => {:topic_type => o.to_s} }}
 end
