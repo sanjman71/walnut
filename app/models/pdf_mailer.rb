0aaa13221342ba246bfa120728094781c1f7a34f
Sanjay <sanjay@jarna.com>
2010-01-25 12:07:18 -0600

    changed pdf mailer

--- app/models/pdf_mailer.rb
+++ app/models/pdf_mailer.rb
@@ -1,9 +1,20 @@
 class PdfMailer < ActionMailer::Base
   
-  def email(address, subject, pdf_file)
+  def email(address, subject, body, pdf_file)
     from(SMTP_FROM)
     recipients(address)
     subject(subject)
+    content_type("multipart/mixed")
+
+    part :content_type => "multipart/alternative" do |a|
+      a.part "text/plain" do |p|
+        p.body = body
+      end
+
+      # a.part "text/html" do |p|
+      #   p.body = render_message 'order_placed.text.html.erb', :purchase => purchase
+      # end
+    end
 
     # create pdf attachment
     attachment "application/pdf" do |a|
