493d75dc9731b20c1b1f852da622595c197ebd49
Sanjay <sanjay@jarna.com>
2010-02-04 00:03:38 -0600

    fixed tls for ruby 1.9 with patch for rails 2.3.3

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :tls => true,
+  :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => "587",
   :authentication => :plain,
+  :domain => "walnutindustries.com",
   :user_name => "messaging@walnutindustries.com",
   :password => "1ndus7ry!"
 }
