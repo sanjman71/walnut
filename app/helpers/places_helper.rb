f4448ef04c68fdd2c96edc8fd4f65df17d6dfc5f
Sanjay <sanjay@jarna.com>
2010-03-30 11:28:19 -0500

    added display_address helper

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,5 +1,10 @@
 module PlacesHelper
 
+  def display_address(location)
+    return "" if location.blank?
+    [location.street_address, city_state_zip(location.city, location.state, location.zip)].reject(&:blank?).join("<br/>")
+  end
+
   def city_state_zip(city, state, zip)
     [city ? "#{city.name}," : "", state ? state.code : "", zip ? zip.name : ""].compact.join(" ")
   end
