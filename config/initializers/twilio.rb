fa21f05ef98d640c6ed049e4128dd32269f7c5c1
Sanjay <sanjay@jarna.com>
2010-04-13 18:50:05 -0500

    added twilio initializer

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,9 @@
+# Twilio REST API version
+API_VERSION   = '2008-08-01'
+
+# Twilio AccountSid and AuthToken
+ACCOUNT_SID   = 'ACd33d0be99d16f3d5d3fcd6ccbca0c949'
+ACCOUNT_TOKEN = 'ea7eb39fa5cb190d9b558fd44454789a'
+
+# Twilio sandbox number
+SMS_CALLERID  = "4155992671"
